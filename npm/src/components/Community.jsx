294718302fdd42ad305b38ee932f61e5649a6b31
chaitanyasai <anumulacs@iitbhilai.ac.in>
2023-07-02 13:43:21 +0530

    made the community page

--- npm/src/components/Community.jsx
+++ npm/src/components/Community.jsx
@@ -2,11 +2,42 @@ import React from "react"
 import '../Community.css'
 import Navbar from './Navbar.jsx'
 import bg from '../assets/Magic_Pattern.png'
+import logo from '../assets/openlake_logo.png'
 
 export default function Community(){
     return(
         <div className="body">
             <Navbar />
+            <p className="cd">$ cd community</p>
+            <div className="card_container1">
+                <div className="glass_card1">
+                    <p>COORDINATORS :</p>
+                    <div className="coordie_card">
+                    <div className="person_one">
+                        <img className="logo1" src={logo} />
+                        <h3>Name:</h3>
+                        <h3>Contact me at :</h3>
+                    </div>
+                    <div className="person_two">
+                        <img className="logo2" src={logo} />
+                        <h3>Name:</h3>
+                        <h3>Contact me at :</h3>
+                    </div>
+                    </div>
+                </div>
+                <div className="glass_card2">
+                    <p>MEMBERS :</p>
+                    <div className="member">
+                        <h3> -{'>'} member 1</h3>
+                        <h3> -{'>'} member 2</h3>
+                    </div>
+                </div>
+            </div>
+            <div className="card_container2">
+                <div className="glass_card3">
+                    <p>PREVIOUS COORDINATORS :</p>
+                </div>
+            </div>
         </div>
     )
 }
