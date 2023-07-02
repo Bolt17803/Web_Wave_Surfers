641752c3e55f772797edacea9d417b485920af0c
HemanthGaddey <gaddeyhemanth@gmail.com>
2023-07-02 07:33:31 +0530

    imporoved line menu, added command prompt

--- npm/src/components/Projects.jsx
+++ npm/src/components/Projects.jsx
@@ -3,15 +3,20 @@ import { useState } from "react";
 import '../Projects.css';
 import Navbar from './Navbar.jsx';
 import bg from '../assets/projects_bg.png';
+import ac from '../assets/projects/activitytracker.png';
 
 export default function Projects(){
     var projects = ['ACTIVITY TRACKER','CANON FORCE','LEADERBOARD PRO','KSP','HOMEWORK SCHEDULER','PHOTO SHARING','CGPA'];
-
+    
     const [project, setProject] = useState(projects[0]);
 
     const Card = ()=>{
         return(
             <div className="card">
+                <h3>{project}</h3>
+                <p>Knowledge sharing platform application aims to get the students acquainted with the courses, professors, evaluation schemes. The said things can be achieved via gathering course feedback, professor feedback, and previous year grading schemes. This can be done by making a portal (just like a blog site) where people can share their honest reviews without being scrutinized much.</p>
+                <button type="button"><a href="#">VIEW SOURCE CODE</a></button>
+                <img src={ac}></img>
             </div>
         );
     };
@@ -40,8 +45,8 @@ export default function Projects(){
 
     return(
         <div className="body">
-            <Navbar />
-            <img className="background" src={bg} />
+            {/* <Navbar /> */}
+            <h1>$ cd ~/ projects/ KSP</h1>
             <Card />
             <LineMenu />
         </div>
