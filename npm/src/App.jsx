605c50906cfb31e21065d8d97032b7a46a3052a6
chaitanyasai <anumulacs@iitbhilai.ac.in>
2023-07-07 14:01:20 +0530

    resolved errors

--- npm/src/App.jsx
+++ npm/src/App.jsx
@@ -2,12 +2,11 @@ import React from 'react'
 import { useState } from 'react'
 import { BrowserRouter ,Routes,Route} from 'react-router-dom';
 import Home from './components/home'
-// import Navbar from './components/Navbar'
 import Blogs from './components/Blogs'
 import Blogcard from './components/Blogcard';
 import Community from './components/Community'
-import Projects from './components/Projects'
-import Programs from './components/Programs'
+// import Projects from './components/Projects'
+// import Programs from './components/Programs'
 
 function App() {
   const apikey="83951c0e1b35448c95fd7924393c9e9a";
@@ -16,8 +15,8 @@ function App() {
     <Routes>
       <Route exact path="/" element={<Home />} />
       <Route exact path="/community" element={<Community />} />
-      <Route exact path="/projects" element={<Projects />} />
-      <Route exact path="/programs" element={<Programs />} />
+      {/* <Route exact path="/projects" element={<Projects />} /> */}
+      {/* <Route exact path="/programs" element={<Programs />} /> */}
       </Routes>     
     </BrowserRouter>
   )
