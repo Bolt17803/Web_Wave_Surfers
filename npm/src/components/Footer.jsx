8fc6df1778876610c00dadb36c4d246d3d4e3bb9
chaitanyasai <anumulacs@iitbhilai.ac.in>
2023-07-11 18:31:26 +0530

    added comments to footer

--- npm/src/components/Footer.jsx
+++ npm/src/components/Footer.jsx
@@ -3,6 +3,7 @@ import '../Footer.css'
 import logo from '../assets/openlake_logo.png'
 import github from '../assets/github_logo.png'
 import { useState } from 'react'
+import * as bsIcons from 'react-icons/bs'
 
 
 export default function Footer(){
@@ -11,20 +12,48 @@ export default function Footer(){
         id:"",
     })
 
+    const [thnx, setThnx] = useState(false)
+
+    const[warning, setWarning] = useState(false)
+
     const handleChange = (event) => {
         setData({...data, id: event.target.value });
+    }
+
+    const validate = (email) => {
+        if (email.trim() !== "" && email.includes('@')){
+            return true
+        }
+        else{ 
+            return false 
+        }
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
 
-        const formData = new FormData();
-        formData.append("user_data", JSON.stringify(data));
-        const mail_data = {method: 'POST', body: formData,};
+        if(validate(data.id)){
+            setWarning(!warning)
+            setThnx(!thnx)
+            const formData = new FormData();
+            formData.append("user_data", JSON.stringify(data));
+            const mail_data = {method: 'POST', body: formData,};
 
-        fetch("http://127.0.0.1:8000/submit", mail_data)
-        .catch(error => console.log(error));
-        setData({id:""})
+            fetch("http://127.0.0.1:8000/submit", mail_data)
+            .catch(error => console.log(error));
+            setData({id:""})
+        }else{
+            if(warning){
+                return none
+            }else{
+                if(thnx){
+                setThnx(!thnx)
+                setWarning(!warning)
+                } else{
+                    setWarning(!warning)
+                }
+        }
+        }
     }
 
     return(
@@ -34,11 +63,17 @@ export default function Footer(){
             </div>
             <div className="input_form">
                 <p>enter your contact mail id :</p>
-                <form onSubmit={handleSubmit}>
+                <form>
                     <input type="email" value={data.id} onChange={handleChange}/>
-                    <input type="submit" className="submit" value="&#10148;" />
+                    <bsIcons.BsArrowRightSquare onClick={handleSubmit} className="submit_button" />
                 </form>
             </div>
+            {warning && <div className="reply">
+                <p className="reply">Please enter correct mail id !</p>
+                </div>}
+            {thnx && <div className="reply">
+                <p className="reply">Thank you for subscribing !</p>
+                </div>}
             <div className="other_links">
                 <a href="https://github.com/OpenLake" className="github_link"> <img src={github} className="github"></img> </a>
             </div>
